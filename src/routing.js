cc19cd645bac30e42bb8388e16c85e615d61586f
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2023-08-23 14:17:09 +0400

    feat: second version for routing with auth

--- src/routing.js
+++ src/routing.js
@@ -2,29 +2,74 @@ import { createRouter, createWebHistory } from 'vue-router';
 import isAuthenticated from "./utils/auth";
 
 import AuthView from "./views/AuthView.vue";
-import ProfileView from "./views/ProfileView.vue";
-import PaymentView from "./views/PaymentView.vue";
+// import ProfileView from "./views/ProfileView.vue";
+// import PaymentView from "./views/PaymentView.vue";
 
+// Version 1
+// const routes = [
+//   {
+//     path: '/',
+//     redirect: () => {
+//       return isAuthenticated() ? '/profile/details' : '/auth/login';
+//     }
+//   },
+//   {
+//     path: '/auth',
+//     component: AuthView,
+//     children: [
+//       { path: 'login', component: () => import('./components/test/LoginComponent.vue') },
+//       { path: 'resendPassword', component: () => import('./components/test/ResendPasswordComponent.vue') },
+//       { path: 'forgot', component: () => import('./components/test/ForgotPasswordComponent.vue') },
+//       // ... other authentication-related routes
+//     ]
+//   },
+//   {
+//     path: '/profile',
+//     component: ProfileView,
+//     children: [
+//       { path: 'details', component: () => import('./components/test/ProfileDetailsComponent.vue') },
+//       { path: 'settings', component: () => import('./components/test/ProfileSettingsComponent.vue') },
+//       // ... other profile-related routes
+//     ],
+//     meta: { requiresAuth: true }
+//   },
+//   {
+//     path: '/payment',
+//     component: PaymentView,
+//     children: [
+//       { path: 'process', component: () => import('./components/test/PaymentProcessComponent.vue') },
+//       { path: 'history', component: () => import('./components/test/PaymentHistoryComponent.vue') },
+//       // ... other payment-related routes
+//     ],
+//     meta: { requiresAuth: true }
+//   },
+//   // ... other top-level routes
+// ];
+
+// Version 2
 const routes = [
-  {
+    {
     path: '/',
     redirect: () => {
-      return isAuthenticated() ? '/profile/details' : '/auth/login';
+      return isAuthenticated() ? '/dashboard' : '/auth/login';
     }
   },
   {
-    path: '/auth',
-    component: AuthView,
+    path: "/auth",
+    component: () => import('./views/AuthView.vue'),
     children: [
       { path: 'login', component: () => import('./components/test/LoginComponent.vue') },
       { path: 'resendPassword', component: () => import('./components/test/ResendPasswordComponent.vue') },
       { path: 'forgot', component: () => import('./components/test/ForgotPasswordComponent.vue') },
-      // ... other authentication-related routes
     ]
   },
   {
+    path: "/dashboard",
+    component: () => import('./views/DashboardView.vue'),
+    children: [
+        {
     path: '/profile',
-    component: ProfileView,
+    component: () => import("./views/ProfileView.vue"),
     children: [
       { path: 'details', component: () => import('./components/test/ProfileDetailsComponent.vue') },
       { path: 'settings', component: () => import('./components/test/ProfileSettingsComponent.vue') },
@@ -32,9 +77,9 @@ const routes = [
     ],
     meta: { requiresAuth: true }
   },
-  {
+    {
     path: '/payment',
-    component: PaymentView,
+    component: () => import("./views/PaymentView.vue"),
     children: [
       { path: 'process', component: () => import('./components/test/PaymentProcessComponent.vue') },
       { path: 'history', component: () => import('./components/test/PaymentHistoryComponent.vue') },
@@ -42,8 +87,10 @@ const routes = [
     ],
     meta: { requiresAuth: true }
   },
-  // ... other top-level routes
-];
+    ],
+    meta: { requiresAuth: true }
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Dashboard View</h1>
+      <RouterView />
+
+  </div>
+</template>
+
+<style>
+
+</style>
